aa2d792e839681243e12d1f8c4097d982267c9c4
imshubham07 <mrshubhamdubey20@gmail.com>
2024-02-27 22:53:20 +0530

    Hello World in java

---
+++ Java/first.java
@@ -0,0 +1,11 @@
+public class first{
+     
+    //for main function shortcut (psvm)
+    public static void main(String[] args) {
+        // For new line you can use println 
+        System.out.println("Hello World" + "Namaste duniya");//concinate 2 string
+        System.out.println("this side shubham Dubey");
+        System.out.println(4+3);
+    }
+
+}
