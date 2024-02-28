8e026c500872bce32e41483d83b34f38cce9e9b3
imshubham07 <mrshubhamdubey20@gmail.com>
2024-02-28 15:39:17 +0530

    Variables in java

--- Java/variables.java
+++ Java/variables.java
@@ -1,32 +1,32 @@
-public class variables {
+// public class variables {
 
-    public static void main(String[] args) {
+//     public static void main(String[] args) {
 
-        // variable  creation //initialisation
-        // int age = 29;
-        // int aGe = 31;
-        // int AGE = 21;
+//         // variable  creation //initialisation
+//         // int age = 29;
+//         // int aGe = 31;
+//         // int AGE = 21;
 
-        //decleare
-        // int age;
-        //definition // initializsation
-        int age = 25;   
-        //manuplation //Updation 
-        age = 13;
+//         //decleare
+//         // int age;
+//         //definition // initializsation
+//         int age = 25;   
+//         //manuplation //Updation 
+//         age = 13;
 
-        // you can write 
-        int marks = 100;
-        int Marks = 45;
-        int _marks = 55;
-        int $marks = 300;
+//         // you can write 
+//         int marks = 100;
+//         int Marks = 45;
+//         int _marks = 55;
+//         int $marks = 300;
 
-        // you can't
-        // int 1marks = 100;
+//         // you can't
+//         // int 1marks = 100;
 
-        // you can write
-        int weight1 = 200;
-        int weight_ = 300;
-        int weight$ = 389;
+//         // you can write
+//         int weight1 = 200;
+//         int weight_ = 300;
+//         int weight$ = 389;
         
-    }
-}
+//     }
+// }
