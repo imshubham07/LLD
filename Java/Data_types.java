c14ce88bcf3c6c5f51f33e9a5d948c5024ef8eb3
imshubham07 <mrshubhamdubey20@gmail.com>
2024-02-28 16:23:30 +0530

    Data types and its Conversion in java

---
+++ Java/Data_types.java
@@ -0,0 +1,54 @@
+public class Data_types {
+    public static void main(String[] args) {
+//          // boolean
+//          /* 
+//          boolean found = true;// it can't convert 1 to true and 0 to mins
+//          boolean isAvilable = false;
+//          System.out.println(found);
+//          System.out.println(isAvilable);*/
+
+
+//          //charecter 
+//          char ch = 'k';
+//          System.out.println("my charecter is => "+ (char)(ch+1));
+
+//         //  intiger => byte, int, short, long
+//         byte marks = 110;
+//         System.out.println("My Marks : "  + marks);
+
+//         short weight = 32000;
+//         System.out.println("My Weight: " + weight);
+
+//         int height = 130000;
+//         System.out.println("My height : " + height);
+
+//         // agar jab bhi ham long use karte hai java me tab valur ke last me (L) use karna padta
+//         long length = 1000000000000000000L;
+//         System.out.println("My Length : "+ length);
+
+
+//         //decimal points
+
+//         //agar jab bhi float value write karte hai tab value ke last me (f) write karna padta hai
+//         float number = 3.14f;
+//         System.out.println("My Floating number is : "+number);
+
+//         double percentage = 99.9855425423;
+//         System.out.println("My Percentage : " + percentage);
+
+// // inplicit conversion in java
+//         byte num1 = 12;
+//         short num2 = num1;
+//         System.out.println(num2);
+//         int num3 = num2;
+//         System.out.println(num3);
+//         long  num4 = num3;
+//         System.out.println(num4);
+
+        //Explicit Conversion
+        //if you convert Explicit conversion than aap (datatype) likhna padta hai conversion ke liye
+        int num1 = 109;
+        char mych = (char)num1;
+        System.out.println(mych);
+    }
+}
