7311412489374a092c275369a11c0c85b725fcaf
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-01 23:50:54 +0530

    Operators in Java

---
+++ Java/Operators.java
@@ -0,0 +1,45 @@
+public class Operators {
+     public static void main(String[] args) {
+        // // Arthmetic 
+        // int num1 = 5;
+        // int num2 = 13;
+        // System.out.println(num1+num2);
+        // System.out.println(num1-num2);
+        // System.out.println(num1*num2);
+        // System.out.println((double)num1/num2);
+        // System.out.println(num1%num2);
+
+        // // Relation Operator 
+        // int a = 10;
+        // int b = 20;
+        // System.out.println(a>b);
+        // System.out.println(a<b);
+        // System.out.println(a<=b);
+        // System.out.println(a>=b);
+        // System.out.println(a!=b);
+        // System.out.println(a==b);
+
+        //Logical Operator
+        int a = 10;
+        int b = 20;
+        int c = 30;
+
+        boolean cond1 = false;
+        boolean cond2 = (a<c);
+        boolean cond3 = (b<c);
+
+        boolean answer1 = cond1 && cond2 &&cond3;
+        System.out.println(answer1);
+        boolean answer2 = cond1||cond2||cond3;
+        System.out.println(answer2 );
+        boolean answer3 = !cond1;
+        System.out.println(answer3);  
+        
+        
+        // Logical Operator(&&,||,!)
+        // Assignment Operator(=,-=,+=,/=,%=)
+        // Unary Increment/Decrement(++,--)
+
+    }
+}
+ 
