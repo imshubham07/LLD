3cccfdb84c87862c59cde8d4e8e977bc800794d9
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-02 23:24:38 +0530

    How to Take input in java

---
+++ Java/input.java
@@ -0,0 +1,22 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        //Creatte a object of  Scanner Class
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = sc.nextInt();
+
+        System.out.println("Enter your Float number: ");
+        float f = sc.nextFloat();
+
+        System.out.println("Enter your big Number: ");
+        BigInteger bg = sc.nextBigInteger();
+
+        System.out.println("My age is: " + age);
+        System.out.println("My Float Number is: " + f);
+        System.out.println("My BigNumber is: " + bg);
+        sc.close();
+    }
+}
