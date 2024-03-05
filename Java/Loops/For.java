979377c1d8ad470075312a2d450b76cc245b98e8
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-05 20:10:56 +0530

    For Loops in java

---
+++ Java/Loops/For.java
@@ -0,0 +1,30 @@
+package Loops;
+
+public class For {
+    public static void main(String[] args) {
+        
+        //For Loops
+
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println("Shubham");
+        // }
+
+
+        // for( int count = 0; count<=10; count++){
+        //     System.out.println(count);
+        // }
+
+        // for (int i = 0; i < 10; i++) {
+        //     System.out.println(i*2);
+        // }
+
+
+        //Nested For Loops
+        for (int i = 1; i <= 3; i++) {
+            for (int j = 1; j <= 2; j++) {
+                System.out.println("i= " + i +"j= "+j);
+            }
+        }
+    }
+}
