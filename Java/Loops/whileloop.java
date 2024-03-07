23d3d0ffeafd4b206e83c61286c3b7f92329bbb3
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-07 10:32:33 +0530

    While Loop in Java

---
+++ Java/Loops/whileloop.java
@@ -0,0 +1,20 @@
+package Loops;
+
+public class whileloop {
+    
+    public static void main(String[] args) {
+        
+    //     int i = 0;
+    //     while (i<=5) {
+    //         System.out.println(i);
+    //         i++;
+    //     }
+
+    int i = 1;
+    while (i<=6) {
+        System.out.println("Shubham");
+        i++;
+    }
+    
+    }
+}
