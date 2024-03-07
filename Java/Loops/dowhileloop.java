d4cf4e2a5f09d2b8bce1310938d1f6ad1da394e4
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-07 10:48:50 +0530

    Do While Loop in Java

---
+++ Java/Loops/dowhileloop.java
@@ -0,0 +1,17 @@
+package Loops;
+
+public class dowhileloop {
+    public static void main(String[] args) {
+        // int i = 0;
+        // do {
+        //     System.out.println(i);
+        //     i++;
+        // } while (i <= 5);
+
+        int i = 0;
+        do {
+            System.out.println("Shubham");
+            i++;
+        } while (i<=5);
+    }
+}
