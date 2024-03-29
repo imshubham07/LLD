06e6d5d92af2d33b5cae5e235cc4c60916f6f7e7
imshubham07 <mrshubhamdubey20@gmail.com>
2024-03-29 14:07:07 +0530

    String and its Some method

---
+++ Java/String/string.java
@@ -0,0 +1,105 @@
+package String;
+
+// import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+
+        //  Some method in java
+
+        String name = " ";
+        System.out.println(name.length());
+        if (name.isBlank()) {
+            System.out.println("string are Blank");
+        }
+        else{
+            System.out.println("string are not Blank");
+        }
+        
+
+        // String name  = "Shubham";
+        // char[] ch = name.toCharArray();
+        // for (char c : ch) {
+        //     System.out.println(c);
+        // }
+
+        // String name = "Love ";
+        // name = name.replace('o', 'a');
+        // System.out.println(name);
+
+        // int num = 543;
+        // String str = String.valueOf(num);
+        // System.out.println(num+1);
+        // System.out.println(str+1);
+        // String str = "My Name is Shubham Dubey";
+        // System.out.println(str.endsWith("Dubey"));
+        // System.out.println(str.startsWith("My"));
+
+        // String[] words  = str.split(" ");
+
+        // for (String word : words) {
+        //     System.out.println(word);
+        // }
+
+        // System.out.println(str.trim());
+        // System.out.println(str.toUpperCase());
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.contains("kunal"));
+        // System.out.println(str.length());
+        // System.out.println(str.charAt(4));
+        // System.out.println(str.substring(0, 7));
+        // System.out.println(str.substring(6));
+
+
+
+
+//Taking input in JavaScript
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter your First Name: ");
+        // String firstName = sc.nextLine();
+
+        // System.out.println("Enter Your Last Name: ");
+        // String lastName = sc.nextLine();
+
+        // System.out.println(firstName + " " + lastName);
+        // sc.close();
+
+
+
+
+        
+        // String lastName = "Dubey";
+        // System.out.println(lastName);
+
+        // String firstName = "Shubham";
+        // System.out.println(firstName);
+
+        // System.out.println(firstName + " " + lastName);
+        // String str = "Dubey";
+        // str = "Shubham";
+        // System.out.println(str);
+
+        // String name1 = "Code";
+        // String name2 = "Code";
+        // String name3 = new String("CODE");
+
+        // if (name1 == name3) { // it cheak (==) only refrence 
+        //     System.out.println("String are equal");
+        // }
+        // else{
+        //     System.out.println("String are not Equal");
+        // }
+        // if (name1.equals(name2)) { // it cheaks Value
+        //     System.out.println("String are equal");
+        // }
+        // else{
+        //     System.out.println("String are not Equal");
+        // }
+        // if (name1.equalsIgnoreCase(name3)) {//agar case sensivity cheak nhi karna ho to use karte hai
+        //     System.out.println("String are equal");
+        // }
+        // else{
+        //     System.out.println("String are not Equal");
+        // }
+    }
+}
